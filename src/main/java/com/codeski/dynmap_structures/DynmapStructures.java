b95da4b3a71240e9285e61d2c067534cdf422bf0
Kevin Breslin <kevin.breslin@icloud.com>
2014-05-28 15:13:44 -0400

    Initial commit with proof of concept

---
+++ src/main/java/com/codeski/dynmap_structures/DynmapStructures.java
@@ -0,0 +1,91 @@
+package com.codeski.dynmap_structures;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.dynmap.DynmapCommonAPI;
+import org.dynmap.markers.MarkerAPI;
+import org.dynmap.markers.MarkerSet;
+
+import com.codeski.nbt.NBTReader;
+import com.codeski.nbt.tags.NBT;
+import com.codeski.nbt.tags.NBTByte;
+import com.codeski.nbt.tags.NBTCompound;
+import com.codeski.nbt.tags.NBTInteger;
+import com.codeski.nbt.tags.NBTList;
+import com.codeski.nbt.tags.NBTString;
+
+public class DynmapStructures extends JavaPlugin implements Listener
+{
+	@Override
+	public void onDisable() {
+		//
+	}
+
+	@Override
+	public void onEnable() {
+		if (Bukkit.getPluginManager().isPluginEnabled("dynmap")) {
+			DynmapCommonAPI api = (DynmapCommonAPI) Bukkit.getPluginManager().getPlugin("dynmap");
+			MarkerAPI markerapi = api.getMarkerAPI();
+			MarkerSet set = markerapi.createMarkerSet("structures", "Structures", null, false);
+			InputStream in = this.getClass().getResourceAsStream("/fortress.png");
+			markerapi.createMarkerIcon("structures.fortress", "Fortress", in);
+			in = this.getClass().getResourceAsStream("/mineshaft.png");
+			markerapi.createMarkerIcon("structures.mineshaft", "Mineshaft", in);
+			in = this.getClass().getResourceAsStream("/stronghold.png");
+			markerapi.createMarkerIcon("structures.stronghold", "Stronghold", in);
+			in = this.getClass().getResourceAsStream("/temple.png");
+			markerapi.createMarkerIcon("structures.temple", "Temple", in);
+			in = this.getClass().getResourceAsStream("/village.png");
+			markerapi.createMarkerIcon("structures.village", "Village", in);
+			in = this.getClass().getResourceAsStream("/witch.png");
+			markerapi.createMarkerIcon("structures.witch", "Witch", in);
+			World world = Bukkit.getWorld("world");
+			File dataFolder = new File(world.getWorldFolder(), "data");
+			for (File f : dataFolder.listFiles())
+				if (!f.getName().equals("villages.dat"))
+					try {
+						NBTReader r = new NBTReader(f);
+						NBTCompound root = r.read();
+						NBTCompound data = (NBTCompound) root.getPayload()[0];
+						NBTCompound features = (NBTCompound) data.getPayload()[0];
+						for (NBT temp : features.getPayload()) {
+							NBTCompound entries = (NBTCompound) temp;
+							String id = "";
+							int x = Integer.MIN_VALUE, z = Integer.MIN_VALUE;
+							boolean isWitch = false;
+							for (NBT entry : entries.getPayload())
+								if (entry.getName().equals("ChunkX"))
+									x = ((NBTInteger) entry).getPayload();
+								else if (entry.getName().equals("ChunkZ"))
+									z = ((NBTInteger) entry).getPayload();
+								else if (entry.getName().equals("id"))
+									id = ((NBTString) entry).getPayload();
+								else if (f.getName().equals("Temple.dat") && entry.getName().equals("Children")) {
+									NBT[] children = ((NBTList) entry).getPayload();
+									if (children.length > 0 && children[0] instanceof NBTCompound) {
+										children = ((NBTCompound) children[0]).getPayload();
+										for (NBT child : children)
+											if (child.getName().equals("Witch"))
+												isWitch = ((NBTByte) child).getPayload() > 0;
+									}
+								}
+							if (x != Integer.MIN_VALUE && z != Integer.MIN_VALUE && id != "")
+								if (id.equals("Fortress"))
+									set.createMarker(id + "," + x + "," + z, id, "world_nether", x * 16, 64, z * 16, markerapi.getMarkerIcon("structures." + id.toLowerCase()), false);
+								else if (isWitch)
+									set.createMarker(id + "," + x + "," + z, "Witch Hut", "world", x * 16, 64, z * 16, markerapi.getMarkerIcon("structures.witch"), false);
+								else
+									set.createMarker(id + "," + x + "," + z, id, "world", x * 16, 64, z * 16, markerapi.getMarkerIcon("structures." + id.toLowerCase()), false);
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+		}
+	}
+}
